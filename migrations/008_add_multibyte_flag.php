e908dff3e48738c6f890d618e11ff3d3c5f88339
Anrijs <anrijs@anrijs.lv>
2026-04-12 07:45:28 +0300

    fix default multibyte flag value

--- migrations/008_add_multibyte_flag.php
+++ migrations/008_add_multibyte_flag.php
@@ -6,7 +6,7 @@ class Migration_008 extends Migration {
     }
 
     function migrate($pdo) {
-        $pdo->exec("ALTER TABLE `contacts` ADD `multibyte` TINYINT NOT NULL DEFAULT '1' AFTER `hash_size`;");
+        $pdo->exec("ALTER TABLE `contacts` ADD `multibyte` TINYINT NOT NULL DEFAULT '0' AFTER `hash_size`;");
 
         return array(
             'success' => true,
